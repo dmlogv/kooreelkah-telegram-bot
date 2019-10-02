3f466a730b522398ca563d45598b9ca2894cba4d
Dmitry Logvinenko <5diezfun@gmail.com>
2019-10-02 12:28:55 +0300

    Fix a docstring position

--- helpers.py
+++ helpers.py
@@ -7,11 +7,11 @@ class Chat:
 class Member(Chat):
     """Telegram chat member"""
 
-    """Get Telegram mention link
-    
-    Args:
-        text: Link text"""
     def as_link(self, text):
+        """Get Telegram mention link
+
+        Args:
+            text: Link text"""
         return f'[{text}](tg://user?id={self.chat_id})'
 
 
