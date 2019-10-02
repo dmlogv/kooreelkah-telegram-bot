06cb933473f3a4c83c8322209232966cd1713c35
Dmitry Logvinenko <5diezfun@gmail.com>
2019-10-02 15:04:41 +0300

    Add group handler

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ try:
 except ImportError:
     raise ImportError('You must to build your own config.py based on config.default.py')
 
-
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 
 
@@ -28,6 +27,19 @@ def error(update, context):
     logging.error(update, context.error)
 
 
+def create_group_handler(group):
+    def group_handler(update, context):
+        mention = group.mention_all()
+        text = ' '.join(context.args)
+
+        update.message.reply_markdown(
+            f'{mention} {text}',
+            quote=False,
+            forward_from_message_id=update.message.forward_from_message_id)
+
+    return group_handler
+
+
 def main():
     updater = Updater(token=config.TOKEN, use_context=True)
     dispatcher = updater.dispatcher
@@ -37,6 +49,10 @@ def main():
     dispatcher.add_handler(CommandHandler('help', help))
 
     dispatcher.add_error_handler(error)
+
+    # Init mention group commands
+    for group in config.GROUPS:
+        dispatcher.add_handler((CommandHandler(group.commands, create_group_handler(group))))
 
     updater.start_polling()
     updater.idle()
